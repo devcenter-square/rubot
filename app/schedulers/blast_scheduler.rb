a9d8fc7d05d811800d53f054d97fe6a103f3df80
Sunday Adefila <adefilaedward@gmail.com>
2017-09-06 11:20:05 +0100

    blasts to users

--- app/schedulers/blast_scheduler.rb
+++ app/schedulers/blast_scheduler.rb
@@ -6,19 +6,19 @@ class BlastScheduler
     current_users_slack_id = api_members_ids & users_slack_id
     client_users = User.where(slack_id: current_users_slack_id)
 
-    me = User.find_by(user_name: "sunday", real_name: "Sunday Adefila")
-
     time = Time.now + 5
-
     rs = Rufus::Scheduler.new(max_work_threads: 1000)
 
     client_users.each do |user|
       # DO NOT SPAM!!! send message at 2sec intervals. https://api.slack.com/docs/rate-limits
       time += 2
       rs.at time do
-        send_blast(me.channel_id, blast.text)
+        send_blast(user.channel_id, blast.text)
       end
     end
+
+    # TODO: Just for debugging, remove this soon
+    me = User.find_by(user_name: "sunday", real_name: "Sunday Adefila")
     time += 2
     rs.at time do
       send_blast(me.channel_id, "DONE!!! Sent blasts to all #{client_users.count} available members of DC-square")
